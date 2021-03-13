ae91d15eb7a77d73bfc9c0186ac74a94721d05cb
Haider <raja@192.168.10.6>
2021-03-13 22:41:48 +0500

    v0.1

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+app.use(express.static(__dirname));
+
+app.get("/", (req, res) => {
+	res.sendFile(__dirname + "/index.html");
+});
+app.listen(5000);
